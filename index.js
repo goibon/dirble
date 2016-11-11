96046e5d19fac54f98776a38782d32e6be66b937
Kasper Lind Sørensen <kasper.lind.sorensen@gmail.com>
2016-11-11 21:39:21 +0100

    Added getRecentlyAddedStations method.

--- index.js
+++ index.js
@@ -31,5 +31,28 @@ module.exports = function (apiKey) {
     })
   }
 
+  /*
+      Returns a list of recently added radio stations
+      @param {number} [page = 0] show which per_page stations to show
+      @param {number} [perPage = 20] How many stations per page to show
+      @param {number} [offset = 0]
+  */
+  dirble.getRecentlyAddedStations = function (page, perPage, offset) {
+    var query = '/stations/recent?' + querystring.stringify({ page: page, per_page: perPage, offset: offset, token: apiKey })
+
+    return new Promise(function (resolve, reject) {
+      request(hostname + query, function (error, response, body) {
+        if (error) {
+          reject(error)
+        }
+        if (response.statusCode !== 200) {
+          reject(response.statusMessage)
+        }
+
+        resolve(body)
+      })
+    })
+  }
+
   return dirble
 }

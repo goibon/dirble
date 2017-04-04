b896e15dfbd29ce67be9bbaaa5d0a69fcbeac2e6
Kasper Lind Sørensen <kasper.lind.sorensen@gmail.com>
2017-04-04 22:09:44 +0200

    Added tests for getRecentlyAddedStations() and increased integration test timeout to 5000ms and slow to 1000ms.

--- test/integration.js
+++ test/integration.js
@@ -2,29 +2,57 @@
 const assert = require('assert')
 const API_KEY = process.env.DIRBLE_API_KEY
 const dirble = require('../index')(API_KEY)
-const defaultPageNumber = 0
 
-describe('Calling getStations() with', function () {
-  it('no arguments returns a list of stations', function () {
-    return dirble.getStations().then((stations) => {
-      assert(stations.length > 0)
+describe('Calling', function () {
+  const defaultPageNumber = 0
+  const offset = 1
+  const defaultPerPage = 2
+  const perPage = 5
+  this.timeout(5000)
+  this.slow(1000)
+
+  describe('getStations() with', function () {
+    it('no arguments returns a list of stations', function () {
+      return dirble.getStations().then((stations) => {
+        assert(stations.length > 0)
+      })
+    })
+
+    it('perPage = 5, returns exactly 5 stations', function () {
+      return dirble.getStations(defaultPageNumber, perPage).then((stations) => {
+        assert.equal(stations.length, perPage)
+      })
+    })
+
+    it('offset = 1, returns stations from specified offset', function () {
+      return dirble.getStations(defaultPageNumber, defaultPerPage).then(function (stations) {
+        var expectedStation = stations[offset]
+        dirble.getStations(defaultPageNumber, defaultPerPage, offset).then(function (stations) {
+          assert.deepStrictEqual(stations[0], expectedStation)
+        })
+      })
     })
   })
 
-  it('perPage = 5, returns exactly 5 stations', function () {
-    const expectedAmountOfStations = 5
-    return dirble.getStations(defaultPageNumber, expectedAmountOfStations).then((stations) => {
-      assert.equal(stations.length, expectedAmountOfStations)
+  describe('getRecentlyAddedStations() with', function () {
+    it('no arguments returns a list of stations', function () {
+      return dirble.getRecentlyAddedStations().then((stations) => {
+        assert(stations.length > 0)
+      })
     })
-  })
 
-  it('offset = 1, returns stations from specified offset', function () {
-    const offset = 1
-    const amountofStations = 2
-    return dirble.getStations(defaultPageNumber, amountofStations).then(function (stations) {
-      var expectedStation = stations[offset]
-      dirble.getStations(defaultPageNumber, amountofStations, offset).then(function (stations) {
-        assert.deepStrictEqual(stations[0], expectedStation)
+    it('perPage = 5, returns exactly 5 stations', function () {
+      return dirble.getRecentlyAddedStations(defaultPageNumber, perPage).then((stations) => {
+        assert.equal(stations.length, perPage)
+      })
+    })
+
+    it('offset = 1, returns stations from specified offset', function () {
+      return dirble.getRecentlyAddedStations(defaultPageNumber, defaultPerPage).then(function (stations) {
+        var expectedStation = stations[offset]
+        dirble.getRecentlyAddedStations(defaultPageNumber, defaultPerPage, offset).then(function (stations) {
+          assert.deepStrictEqual(stations[0], expectedStation)
+        })
       })
     })
   })
